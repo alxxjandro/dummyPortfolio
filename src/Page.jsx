eac633e9eb18e7742b82fb84861e95aae15a814c
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-07-04 16:33:00 -0600

    footers ready, going to start working on the media querys, but besides that the design itself it's finished

--- src/Page.jsx
+++ src/Page.jsx
@@ -61,8 +61,31 @@ function Content(){
 
 function Footer(){
     return(
-        <div>
+        <div className='footer'>
+            <div className='footerInfo'>
+                <div className='contactInfo'>
+                    <h1>Contact me</h1>
+                    <p>Please get in touch if you think our work could be mutually beneficial!</p>
+                    <p>1234 Random Road<br/>Random Town, California, 12345</p>
+                </div>
+                <div className='phoneAndMail'>
+                    <div className='footerIcon'>
+                        <svg className='aboutMeIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>phone</title><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
+                        <p>555-555-5555</p>
+                    </div>
+                    <div className='footerIcon'>
+                        <svg className='aboutMeIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>email</title><path d="M5,5H18C19.66,5 21,6.34 21,8V17C21,18.66 19.66,20 18,20H5C3.34,20 2,18.66 2,17V8C2,6.34 3.34,5 5,5M5,6C4.5,6 4.06,6.17 3.72,6.47L11.5,11.5L19.28,6.47C18.94,6.17 18.5,6 18,6H5M11.5,12.71L3.13,7.28C3.05,7.5 3,7.75 3,8V17C3,18.1 3.9,19 5,19H18C19.1,19 20,18.1 20,17V8C20,7.75 19.95,7.5 19.87,7.28L11.5,12.71Z" /></svg>
+                        <p>miles.morales@fakemail.com</p>
+                    </div>
+                </div>
+                <div className='footerSocials'>
+                    <img className='aboutMeIcon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+                    <img className='aboutMeIcon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" />
+                    <img className='aboutMeIcon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/twitter/twitter-original.svg" />
+                </div>
+            </div>
             <img
+            className='footerImg'
             srcSet={`
                 ${footer480} 480w,
                 ${footer768} 768w,
@@ -76,7 +99,6 @@ function Footer(){
             src={footer1024}
             alt="Miles morales throwing a web"
             />
-            FOOTER
         </div>
     )
 }
