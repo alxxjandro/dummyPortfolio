cca202a829570b9ab7406b325e5e7d997d94510d
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-07-04 12:49:29 -0600

    'my work' section's done

--- src/Page.jsx
+++ src/Page.jsx
@@ -40,10 +40,63 @@ function Header() {
   );
 }
 
+function Content(){
+    return(
+        <div className='contentDiv'>
+            <h1 className='workTitle'>My work</h1>
+            <div className='projects'>
+                <Project/>  
+                <Project/>  
+                <Project/>  
+                <Project/>  
+                <Project/>  
+                <Project/>  
+            </div>
+        </div>
+    )
+}
+
+function pickRandomColor(){
+    const colors = [
+    '#C0392B', 
+    '#8E44AD', 
+    '#27AE60', 
+    '#D2527F', 
+    '#D35400', 
+    '#2980B9', 
+    '#F39C12', 
+    '#16A085', 
+    '#2C3E50', 
+    '#7D3C98'  
+    ];
+
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function Project({name = 'Project Name', desc = 'Short description of the project. Just a couple sentences will do.' }){
+    let color = pickRandomColor();
+    return(
+        <div className='project'>
+            <div className='projectImg' style={{ backgroundColor: color }}>screenshot <br/>of project</div>
+            <div className='projectName'>
+                <h4>{name}</h4> 
+                <div>        
+                    <img className='projectIcon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+                    <svg className='projectIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg>
+                </div>
+            </div>
+            <div className='projectDesc'>
+                <p>{desc}</p>
+            </div>
+        </div>
+    )
+}
+
 function Page() {
   return (
     <div>
         <Header/>
+        <Content/>
     </div>
   );
 }
